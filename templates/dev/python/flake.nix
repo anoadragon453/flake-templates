94fc669d7daea1c474b93995037830ec03369faf
Andrew Morgan <andrew@amorgan.xyz>
2022-12-19 02:05:52 +0000

    fixup python

--- templates/dev/python/flake.nix
+++ templates/dev/python/flake.nix
@@ -1,24 +1,22 @@
 {
   description = "A Python development environment";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = forAllSystems (system: {
-        # We use mkShellNoCC as we don't need a C compiler here
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-	    jetbrains.pycharm-community
-            poetry
-          ];
-        };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+	  jetbrains.pycharm-community
+          poetry
+        ];
+      };
+    });
 }
