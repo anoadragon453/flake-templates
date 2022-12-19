4ed7ae564d91108c1ba3ff06e56a009ad3248cb7
Andrew Morgan <andrew@amorgan.xyz>
2022-12-19 02:02:42 +0000

    Point to python flake template from flake.nix in the repo root
    This makes it easy to init a template without needing to do directory traversal

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A collection of project templates";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    {
+      templates = {
+        meta = {
+          path = ./templates/dev/python;
+          description = "Simple python project";
+        };
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      }
+    );
+}
